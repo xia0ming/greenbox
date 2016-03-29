dd2190740330e9f1674e86c625f37bd693e223a3
gaojiuli <gaojiuli@gmail.com>
2016-03-29 18:54:34 +0800

    2 day

--- greenbox.py
+++ greenbox.py
@@ -1,2 +1,7 @@
-import os
-os.system()
+import datetime
+from random import randint
+
+now = datetime.datetime.now().replace(year=2016)
+
+print(now + datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24)))
+# os.system()
