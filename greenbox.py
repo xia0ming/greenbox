ce55df983eea60617d9a02481332ad8f1f0022d4
gaojiuli <gaojiuli@gmail.com>
2016-03-31 08:24:00 +0800

    Update 2016-03-31T08:24:00.028534

--- greenbox.py
+++ greenbox.py
@@ -6,19 +6,23 @@ now = datetime.datetime.now()
 start = now.replace(year=2016)
 
 
-def change_time():
+def change_day():
     return datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
 
 
-commit_date = (start + change_time())
+def change_time():
+    return datetime.timedelta(seconds=randint(0, 60), minutes=randint(0, 60))
+
+
+commit_date = (start + change_day())
 times = randint(5, 30)
 
 while commit_date < now:
+    f = open('data.txt', 'a+')
+    commit_date = commit_date + change_day()
     for i in range(times):
-        f = open('data.txt', 'a+')
         commit_date = commit_date + change_time()
         f.writelines(commit_date.isoformat() + '\n')
         f.close()
         os.system('git add .')
         os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
-
